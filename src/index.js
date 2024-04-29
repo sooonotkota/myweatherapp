00544d6819f8975dc5d4b8a6a2d72f80c8c6c3f4
sooonotkota <dakotart08@icloud.com>
2024-04-29 18:33:54 -0500

    adding api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+function refreshWeather(response) {
+    
+}
+
+function searchCity(city) {
+  let apiKey = "c5fb501012566e9bd47a3bo39e25tb37";
+  let apiUrl =
+    'https://api.shecodes.io/weather/v1/current?query=${city}&key=${c5fb501012566e9bd47a3bo39e25tb37}&units=imperial"';
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
