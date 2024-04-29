1926c126a68c57803171d2065bd433091d3d17d7
sooonotkota <dakotart08@icloud.com>
2024-04-29 18:49:26 -0500

    finishing temp api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 function refreshWeather(response) {
 let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = response.data.temperature.current;
+let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#city");
+  
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
@@ -12,10 +16,11 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Chicago");
